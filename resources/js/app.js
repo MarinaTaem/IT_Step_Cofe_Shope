69596b00b405dae29b53f28c7816cf1b34157559
MarinaTaem <taemmarina@gmail.com>
2025-05-24 14:12:49 +0700

    init project

---
+++ resources/js/app.js
@@ -0,0 +1,15 @@
+import './bootstrap';
+import './vendor/jquery-2.2.4.min.js';
+import './vendor/bootstrap.min.js';
+import './easing.min.js';
+import './superfish.min.js';
+import './jquery.ajaxchimp.min.js';
+import './jquery.magnific-popup.min.js';
+import './owl.carousel.min.js';
+import './jquery.sticky.js';
+import './jquery.nice-select.min.js';
+import './parallax.min.js';
+import './waypoints.min.js';
+import './jquery.counterup.min.js';
+import './mail-script.js';
+
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/home', function () {
+    return view('index');
+})->name('home');
+
+Route::get('/generic', function () {
+    return view('generic');
+})->name('generic');
+
+Route::get('/elements', function () {
+    return view('elements');
+})->name('elements');
